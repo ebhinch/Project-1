c7e027e4d03fae72177e45f73485dc84c82e1563
ebhinch <erica.hinchman@gmail.com>
2017-09-11 20:38:15 -0400

    updated game files

--- game.js
+++ game.js
@@ -7,3 +7,29 @@ const minimumAge = {
 
     }
 };
+
+//Create card deck
+const suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+const deck = new Array();
+
+function createDeck() {
+    deck = new Array()
+    for (let i = 0; i < values.length; i++) {
+        for (let j = 0; j < suits.length; j++) {
+            let weight = parseInt(values[i]);
+            if (values[i] === "J" || values[i] === "Q" || values[i] === "K") {
+                weight = 10;
+            }
+            if (values[i] = "A") {
+                weight = 11;
+            }
+            else (weight = values[i]);
+            //push value, suit and weight to an array
+            let card = { Value: values[i], Suit: suits[j], Weight: weight};
+            deck.push(card);
+        }
+    };
+}
+
+//randomiz order cards are dealt in
